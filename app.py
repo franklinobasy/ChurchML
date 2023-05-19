dd7aaf096454070c20ef10ebe0541acc88a4ffa6
Franklin Obasi <franklinobasy@gmail.com>
2023-05-19 14:14:43 +0100

    Added description to app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pickle
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template_string
 from flasgger import Swagger
 import pandas as pd
 
@@ -10,8 +10,38 @@ swagger = Swagger(app)
 app.config['SWAGGER'] = {
     'title': 'ChurchML',  
     'description': 'An API for predicting Church Average Monthly Attendance and Income', 
-    'uiversion': 3 
+    'uiversion': 3,
+    'favicon': 'https://churchml.onrender.com/favicon.png'
 }
+
+
+@app.route('/apidocs/')
+def swagger_ui():
+    template = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>{{ title }}</title>
+        <link rel="icon" type="image/x-icon" href="{{ favicon }}">
+        {{ swagger_css }}
+    </head>
+    <body>
+        <h2>{{ description }}</h2>
+        {{ swagger_body }}
+        {{ swagger_js }}
+    </body>
+    </html>
+    """
+
+    rendered_template = render_template_string(template,
+                                              title=app.config['SWAGGER']['title'],
+                                              favicon=app.config['SWAGGER']['favicon'],
+                                              description=app.config["SWAGGER"]["description"],
+                                              swagger_css=swagger.css(),
+                                              swagger_body=swagger.html(),
+                                              swagger_js=swagger.js())
+
+    return rendered_template
 
 
 @app.route('/predict-income', methods=['POST'])
