0d55bec6846698ea8c84b6ec536964f6a0cdf91e
Franklin Obasi <franklinobasy@gmail.com>
2023-10-25 05:59:41 +0100

    updated models

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from flask import (
 )
 from flasgger import Swagger
 import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 swagger = Swagger(app)
@@ -20,6 +21,22 @@ app.config['SWAGGER'] = {
     'favicon': 'https://churchml.onrender.com/favicon.png'
 }
 
+
+with open('./predict_income.pkl', 'rb') as f:
+  double_exp = pickle.load(f)
+
+with open('./predict_attendance.pkl', 'rb') as f:
+  ar = pickle.load(f)
+
+with open('./scaler.pkl', 'rb') as f:
+  scaler = pickle.load(f)
+  
+# fitted or previous
+forecast_steps = 37  # Adjust the number of steps you want to forecast backward
+ar_forecast_backwards = ar.predict(start=-forecast_steps, end=-1)
+
+forecast_steps = 936  # Adjust the number of steps you want to forecast backward
+ar_forecast_forewards = ar.forecast(forecast_steps)   
 
 @app.route('/predict-income', methods=['POST'])
 def predict_income():
@@ -59,13 +76,31 @@ def predict_income():
     month = data['month']
     year = data['year']
 
-    with open('./predict_income.pkl', 'rb') as f:
-        double_exp = pickle.load(f)
+    # fitted or previous
+    forecast_steps = 37  # Adjust the number of steps you want to forecast backward
+    forecast_backwards = double_exp.predict(start=-forecast_steps, end=-1)
+    forecast_backwards = scaler.inverse_transform(forecast_backwards.values.reshape(-1, 1))
 
-    time_series = double_exp.forecast(100)
+    forecast_steps = 936  # Adjust the number of steps you want to forecast backward
+    forecast_forewards = double_exp.forecast(forecast_steps)
+    forecast_forewards = scaler.inverse_transform(forecast_forewards.values.reshape(-1, 1))
+
+    values = np.concatenate((forecast_backwards, forecast_forewards)).flatten()
+    index = np.concatenate((ar_forecast_backwards.index, ar_forecast_forewards.index))
+    
+    time_series = pd.Series(values, index=index)
 
     date = f'{year}-{month}-01'
-    value = time_series[date]
+    try:
+      value = time_series[date]
+    except KeyError:
+      return jsonify(
+        {
+            'status': "ok",
+            "date": date,
+            'value': f"Not trained"
+        }
+      )
 
     if value is None:
         return jsonify({'error': 'Value not found for the specified month and year.'}), 404
@@ -117,13 +152,22 @@ def predict_attendance():
     month = data['month']
     year = data['year']
 
-    with open('./predict_attendance.pkl', 'rb') as f:
-        ar = pickle.load(f)
-
-    time_series = ar.forecast(100)
+    values = np.concatenate((ar_forecast_backwards, ar_forecast_forewards))
+    index = np.concatenate((ar_forecast_backwards.index, ar_forecast_forewards.index))
+    
+    time_series = pd.Series(values, index=index)
 
     date = f'{year}-{month}-01'
-    value = time_series[date]
+    try:
+      value = time_series[date]
+    except KeyError:
+      return jsonify(
+        {
+            'status': "ok",
+            "date": date,
+            'value': f"Not trained"
+        }
+      )
 
     if value is None:
         return jsonify({'error': 'Value not found for the specified month and year.'}), 404
@@ -147,7 +191,7 @@ def income():
     return render_template(
       "income.html",
       url_for=url_for,
-      years=range(2023, 2100),
+      years=range(2019, 2100),
       months=range(1, 13)
     )
   
@@ -157,7 +201,7 @@ def attendance():
     return render_template(
       "attendance.html",
       url_for=url_for,
-      years=range(2023, 2100),
+      years=range(2019, 2100),
       months=range(1, 13)
     )
 
